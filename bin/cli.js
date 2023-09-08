4fce554c08fd5a8c05c01c130f66108f03180d34
adityakmr7 <adityakmr9672@gmail.com>
2023-09-08 12:12:06 +0530

    fix: updated the cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,43 +1,57 @@
 #!/usr/bin/env node
-const {execSync} = require('child_process');
-const {promisify} = require('util');
-const fs = require('fs');
-const path = require('path');
+const { execSync } = require("child_process");
+const { promisify } = require("util");
+const fs = require("fs");
+const path = require("path");
 const rm = promisify(fs.rm);
-const runCommand = command => {
-    try {
-        execSync(`${command}`, {stdio:'inherit'});
-    }catch(e) {
-        console.error(`Failed to execute ${command}`,e);
-        return false
-    }
-    return true
-}
-let defaultRepoName = 'new-project'
+const runCommand = async (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (e) {
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+};
+let defaultRepoName = "new-project";
 let repoName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, repoName);
-if(!repoName) {
-    console.log(`Since you did not provide a project name. We are calling it ${defaultRepoName}`);
-    repoName = defaultRepoName;
+if (!repoName) {
+  console.log(
+    `Since you did not provide a project name. We are calling it ${defaultRepoName}`
+  );
+  repoName = defaultRepoName;
 }
-const getCheckoutCommand = `git clone --depth 1 https://github.com/adityakmr7/create-react-chrome-extension ${repoName}`;
-const rmGit = rm(path.join(projectPath, ".git"), { recursive: true, force: true });
-// remove the installation file
-const rmBin = rm(path.join(projectPath, "bin"), { recursive: true, force: true });
-await Promise.all([rmGit, rmBin]);
-const installDepsCommand = `cd ${repoName} && npm install`;
-console.log(`Cloning the repository with name ${repoName}`);
+try {
+  const getCheckoutCommand = `git clone --depth 1 https://github.com/adityakmr7/create-react-chrome-extension ${repoName}`;
+  const rmGit = rm(path.join(projectPath, ".git"), {
+    recursive: true,
+    force: true,
+  });
+  // remove the installation file
+  const rmBin = rm(path.join(projectPath, "bin"), {
+    recursive: true,
+    force: true,
+  });
+  await Promise.all([rmGit, rmBin]);
+  const installDepsCommand = `cd ${repoName} && npm install`;
+  console.log(`Cloning the repository with name ${repoName}`);
 
-const checkout =runCommand(getCheckoutCommand);
-if(!checkout) process.exit(-1);
+  const checkout = runCommand(getCheckoutCommand);
+  if (!checkout) process.exit(-1);
 
-console.log(`Installing dependencies for ${repoName}`)
-const installedDeps = runCommand(installDepsCommand);
+  console.log(`Installing dependencies for ${repoName}`);
+  const installedDeps = runCommand(installDepsCommand);
 
-if(!installedDeps) process.exec(-1);
-console.info('Congrats! You have successfully created the boilerplate');
+  if (!installedDeps) process.exec(-1);
+  console.info("Congrats! You have successfully created the boilerplate");
 
-console.info(`Run command cd ${repoName}`);
+  console.info(`Run command cd ${repoName}`);
 
-console.info(`Happy Hacking!`)
+  console.info(`Happy Hacking!`);
+} catch (e) {
+  // clean up in case of error, so the user does not have to do it manually
+  fs.rmSync(projectPath, { recursive: true, force: true });
+  console.log(e);
+}
