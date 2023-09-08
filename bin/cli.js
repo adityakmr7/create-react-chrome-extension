f08c624242bf139a1d91755a5e31568351d65a7d
adityakmr7 <adityakmr9672@gmail.com>
2023-09-08 14:47:02 +0530

    fix: updated the cli

--- bin/cli.js
+++ bin/cli.js
@@ -23,35 +23,44 @@ if (!repoName) {
   );
   repoName = defaultRepoName;
 }
-try {
-  const getCheckoutCommand = `git clone --depth 1 https://github.com/adityakmr7/create-react-chrome-extension ${repoName}`;
-  const rmGit = rm(path.join(projectPath, ".git"), {
-    recursive: true,
-    force: true,
+const start = async () => {
+  try {
+    const getCheckoutCommand = `git clone --depth 1 https://github.com/adityakmr7/create-react-chrome-extension ${repoName}`;
+    const rmGit = rm(path.join(projectPath, ".git"), {
+      recursive: true,
+      force: true,
+    });
+    // remove the installation file
+    const rmBin = rm(path.join(projectPath, "bin"), {
+      recursive: true,
+      force: true,
+    });
+    await Promise.all([rmGit, rmBin]);
+    const installDepsCommand = `cd ${repoName} && npm install`;
+    console.log(`Cloning the repository with name ${repoName}`);
+
+    const checkout = runCommand(getCheckoutCommand);
+    if (!checkout) process.exit(-1);
+
+    console.log(`Installing dependencies for ${repoName}`);
+    const installedDeps = runCommand(installDepsCommand);
+
+    if (!installedDeps) process.exec(-1);
+    console.info("Congrats! You have successfully created the boilerplate");
+
+    console.info(`Run command cd ${repoName}`);
+
+    console.info(`Happy Hacking!`);
+  } catch (e) {
+    // clean up in case of error, so the user does not have to do it manually
+    fs.rmSync(projectPath, { recursive: true, force: true });
+    console.log(e);
+  }
+};
+start()
+  .then(() => {
+    console.info(`Happy Hacking!`);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-  // remove the installation file
-  const rmBin = rm(path.join(projectPath, "bin"), {
-    recursive: true,
-    force: true,
-  });
-  await Promise.all([rmGit, rmBin]);
-  const installDepsCommand = `cd ${repoName} && npm install`;
-  console.log(`Cloning the repository with name ${repoName}`);
-
-  const checkout = runCommand(getCheckoutCommand);
-  if (!checkout) process.exit(-1);
-
-  console.log(`Installing dependencies for ${repoName}`);
-  const installedDeps = runCommand(installDepsCommand);
-
-  if (!installedDeps) process.exec(-1);
-  console.info("Congrats! You have successfully created the boilerplate");
-
-  console.info(`Run command cd ${repoName}`);
-
-  console.info(`Happy Hacking!`);
-} catch (e) {
-  // clean up in case of error, so the user does not have to do it manually
-  fs.rmSync(projectPath, { recursive: true, force: true });
-  console.log(e);
-}
